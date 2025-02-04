904f688f8e6e2f2349ee1b6dc9ffa0ab8fbfe7da
vitus <129135049+viitus@users.noreply.github.com>
2025-02-03 21:26:13 -0300

    Update 2IMC.py

--- Lista4POO/2IMC.py
+++ Lista4POO/2IMC.py
@@ -1,10 +1,9 @@
 def calcular_imc(peso, altura):
-    imc = peso / altura**2
-    return imc
+    return  peso / altura**2
 
 print ("Cálculo do IMC")
 peso = float(input("Digite o seu peso (kg): "))
-altura = float(input("Digite a sua altura (cm): ")) / 100
+altura = float(input("Digite a sua altura (m): ")) 
 
 if peso <= 0 or altura <= 0:
     print("Peso e altura devem ser valores positivos.")
