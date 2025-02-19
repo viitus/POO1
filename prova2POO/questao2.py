eb2d79cb587bb336b4f32d6f4f0bb91df44c4f2a
vitus <129135049+viitus@users.noreply.github.com>
2025-02-18 22:28:09 -0300

    Update questao2.py

--- prova2POO/questao2.py
+++ prova2POO/questao2.py
@@ -7,24 +7,18 @@ class Corredor:
         elif self.distancia == 42:
             return "maratona"
         return f"{self.distancia} km de corrida"
-
-
 class Nadador:
     def __init__(self, distancia, tipo):
         self.distancia = distancia  # Mantemos o nome distancia
         self.tipo = tipo
     def provaNadar(self):
         return f"{self.distancia} m de nado no {self.tipo}"
-
-
 class Pedalar:
     def __init__(self, distancia, tipo):
         self.distancia = distancia  # Mantemos o nome distancia
         self.tipo = tipo
     def provaPedalar(self):
         return f"{self.distancia} km de ciclismo na {self.tipo}"
-
-
 class TriAtleta(Corredor, Nadador, Pedalar):
     def __init__(self, nome, distancia_corrida, distancia_nado, tipo_nado, distancia_bike, tipo_bike):
         Corredor.__init__(self, distancia_corrida)
@@ -38,7 +32,6 @@ class TriAtleta(Corredor, Nadador, Pedalar):
         # Usa os atributos armazenados para fazer a verificação do Ironman
         if self.distancia_corrida == 42 and self.distancia_nado == 3000 and self.distancia_bike == 120:
             return "A prova será um Ironman."
-        
         # Caso contrário, retorna os detalhes da prova
         return f"A prova será {self.provaCorrer()}, {self.provaNadar()} e {self.provaPedalar()}."
 
